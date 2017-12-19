8678633bf952b173908d4b715f439fdbe4a3da33
Hellux <hellux07@gmail.com>
2017-12-19 22:36:07 +0100

    added skeletons for xdg-shell functions

--- Makefile
+++ Makefile
@@ -1,15 +1,20 @@
-CC = cc
 WL_PROTOCOL = /usr/share/wayland/wayland.xml
+WL_UNSTABLE = /usr/share/wayland-protocols/unstable
+SERVER_SRC = server.c xdg_shell_unstable_v6_server.c
 
-server: server.c
+server: ${SERVER_SRC}
 	wayland-scanner server-header ${WL_PROTOCOL} wl_server.h
-	${CC} server.c -lwayland-server -o server
-	make clean
+	wayland-scanner \
+		server-header \
+		${WL_UNSTABLE}/xdg-shell/xdg-shell-unstable-v6.xml \
+		xdg_shell_unstable_v6_server.h
+
+	cc ${SERVER_SRC} -lwayland-server -o server
 
 client: client.c
 	wayland-scanner client-header ${WL_PROTOCOL} wl_client.h
-	${CC} client.c -lwayland-client -o client
+	cc client.c -lwayland-client -o client
 	make clean
 
 clean:
-	rm -f wl_client.h wl_server.h
+	rm -f wl_client.h wl_server.h xdg_shell_unstable_v6_server.h
---
+++ xdg_shell_unstable_v6_server.c
@@ -0,0 +1,155 @@
+#include "xdg_shell_unstable_v6_server.h"
+
+/* Below functions are documented in xdg-shell-unstable-v6.xml protocol
+ * file. */
+
+void surface_destroy(struct wl_client *client, struct wl_resource *resource)
+{
+    
+}
+
+void surface_get_toplevel(struct wl_client *client,
+                          struct wl_resource *resource, uint32_t id)
+{
+    
+}
+
+void surface_get_popup(struct wl_client *client, struct wl_resource *resource,
+                       uint32_t id, struct wl_resource *parent,
+                       struct wl_resource *positioner)
+{
+
+}
+
+void surface_set_window_geometry(struct wl_client *client,
+                                 struct wl_resource *resource,
+                                 int32_t x, int32_t y,
+                                 int32_t width, int32_t height)
+{
+
+}
+
+void surface_ack_configure(struct wl_client *client,
+                           struct wl_resource *resource, uint32_t serial)
+{
+
+}
+
+void toplevel_destroy(struct wl_client *client, struct wl_resource *resource)
+{
+
+}
+
+void toplevel_set_parent(struct wl_client *client,
+                         struct wl_resource *resource,
+                         struct wl_resource *parent)
+{
+
+}
+                         
+
+void toplevel_set_title(struct wl_client *client, struct wl_resource *resource,
+                        const char *title)
+{
+    
+}
+
+void toplevel_app_id(struct wl_client *client, struct wl_resource *resource,
+                     const char *app_id)
+{
+
+}
+
+void toplevel_show_window_menu(struct wl_client *client,
+                               struct wl_resource *resource,
+                               struct wl_resource *seat,
+                               uint32_t serial, int32_t x, int32_t y)
+{
+
+}
+
+void toplevel_move(struct wl_client *client,
+                   struct wl_resource *resource,
+                   struct wl_resource *seat,
+                   uint32_t serial)
+{
+
+}
+
+void toplevel_resize(struct wl_client *client,
+                     struct wl_resource *resource,
+                     struct wl_resource *seat,
+                     uint32_t serial, uint32_t edges)
+{
+
+}
+
+void toplevel_set_max_size(struct wl_client *client,
+                           struct wl_resource *resource,
+                           int32_t width, int32_t height)
+{
+
+}
+
+void toplevel_set_min_size(struct wl_client *client,
+                           struct wl_resource *resource,
+                           int32_t width, int32_t height)
+{
+
+}
+
+void toplevel_set_maximized(struct wl_client *client,
+                            struct wl_resource *resource)
+{
+
+}
+
+void toplevel_unset_maximized(struct wl_client *client,
+                              struct wl_resource *resource)
+{
+
+}
+
+void toplevel_set_fullscreen(struct wl_client *client,
+                             struct wl_resource *resource,
+                             struct wl_resource *output)
+{
+
+}
+
+void toplevel_unset_fullscreen(struct wl_client *client,
+                               struct wl_resource *resource)
+{
+
+}
+
+void toplevel_set_minimized(struct wl_client *client,
+                            struct wl_resource *resource)
+{
+
+}
+
+struct zxdg_surface_v6_interface surface_interface = {
+    .destroy = surface_destroy,
+    .get_toplevel = surface_get_toplevel,
+    .get_popup = surface_get_popup,
+    .set_window_geometry = surface_set_window_geometry,
+    .ack_configure = surface_ack_configure,
+};
+
+struct zxdg_toplevel_v6_interface toplevel_interface = {
+    .destroy = toplevel_destroy,
+    .set_parent = toplevel_set_parent,
+    .set_title = toplevel_set_title,
+    .set_app_id = toplevel_app_id,
+    .show_window_menu = toplevel_show_window_menu,
+    .move = toplevel_move,
+    .resize = toplevel_resize,
+    .set_max_size = toplevel_set_max_size,
+    .set_min_size = toplevel_set_min_size,
+    .set_maximized = toplevel_set_maximized,
+    .unset_maximized = toplevel_unset_maximized,
+    .set_fullscreen = toplevel_set_fullscreen,
+    .unset_fullscreen = toplevel_unset_fullscreen,
+    .set_minimized = toplevel_set_minimized,
+};
